7a51e244c2caaec903fc1fc1e8f70580764d0dfa
hoshikira18 <khuyen.dev183@gmail.com>
2025-04-15 16:00:35 +0700

    chore: fix saved track status

--- src/components/pages/ArtistDetailPage.tsx
+++ src/components/pages/ArtistDetailPage.tsx
@@ -190,19 +190,14 @@ const ArtistSongCard = ({ track, no }: { track: SpotifyTrack; no: number }) => {
 
 const ArtistAlbums = () => {
     const { artistId } = useParams();
-    const { data: albums, isLoading } = useArtistAlbums(artistId as string);
+    const { data: albums } = useArtistAlbums(artistId as string);
 
     return (
         <div className="px-5 my-10">
             <span className="text-2xl font-bold">Albums</span>
             <div className="flex overflow-x-scroll pb-5">
                 {albums?.map((album) => (
-                    <MediaCard
-                        key={album.id}
-                        title={album.name}
-                        isLoading={isLoading}
-                        imageSrc={album.images[1].url}
-                    />
+                    <MediaCard key={album.id} title={album.name} imageSrc={album.images[1].url} />
                 ))}
             </div>
         </div>
