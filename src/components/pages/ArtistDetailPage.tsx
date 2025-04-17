03f320b6ceab3ffd5cfd90fc07f87989dfd61bb9
hoshikira18 <khuyen.dev183@gmail.com>
2025-04-17 09:10:52 +0700

    fix: reload artist-detail-page

--- src/components/pages/ArtistDetailPage.tsx
+++ src/components/pages/ArtistDetailPage.tsx
@@ -242,10 +242,8 @@ const TopTracksTable = () => {
 
     const handlePlayTrack = async (track: SpotifyTrack) => {
         playbackState.isPaused && currentTrack?.id === track.id
-            ? togglePlay() :
-            await TrackServices.play([track.uri] as string[]).then(() => {
-                setCurrentPlayingTrack(track)
-            })
+            ? togglePlay()
+            : await TrackServices.play([track.uri] as string[]);
     };
 
     const trackContext = useContext(TrackContext);
@@ -253,8 +251,8 @@ const TopTracksTable = () => {
     if (!trackContext || !playerContext) {
         throw new Error("TrackContext or PlayerContext is not available");
     }
-    const { setSavedTracks, savedTracks, currentPlayingTrack, setCurrentPlayingTrack } = trackContext;
-    const { currentTrack, togglePlay, playbackState } = playerContext
+    const { setSavedTracks, savedTracks } = trackContext;
+    const { currentTrack, togglePlay, playbackState } = playerContext;
 
     const handleSaveTrack = async (track: SpotifyTrack) => {
         try {
@@ -299,7 +297,7 @@ const TopTracksTable = () => {
     const isPlaying = (track: SpotifyTrack) => {
         if (!currentTrack) return false;
         return currentTrack.id === track.id;
-    }
+    };
     return (
         <CustomTable>
             <CustomTable.Body>
@@ -307,36 +305,48 @@ const TopTracksTable = () => {
                     <CustomTable.Row key={track.id} className="group">
                         <CustomTable.Cell width={20}>
                             <div className="flex items-center space-x-3">
-                                {
-                                    isPlaying(track) && !playbackState.isPaused ? (
-                                        <div className="group">
-                                            <img width="14" height="14" alt="" src="https://open.spotifycdn.com/cdn/images/equaliser-green.f8937a92.svg" className="group-hover:hidden" />
-                                            <button onClick={togglePlay} type="button" className="hidden group-hover:block">
-                                                <Pause />
-                                            </button>
-                                        </div>
-                                    ) : (
-                                        <div className={isPlaying(track) ? "text-green-400" : "text-zinc-200"}>
-                                            <span className="block w-5 text-center text-base group-hover:hidden">
-                                                {index + 1}
-                                            </span>
-                                            <button
-                                                type="button"
-                                                className="w-5 text-center text-base hidden group-hover:block"
-                                                onClick={() => handlePlayTrack(track)}
-                                            >
-                                                <Play />
-                                            </button>
-                                        </div>
-                                    )
-                                }
+                                {isPlaying(track) && !playbackState.isPaused ? (
+                                    <div className="group">
+                                        <img
+                                            width="14"
+                                            height="14"
+                                            alt=""
+                                            src="https://open.spotifycdn.com/cdn/images/equaliser-green.f8937a92.svg"
+                                            className="group-hover:hidden"
+                                        />
+                                        <button
+                                            onClick={togglePlay}
+                                            type="button"
+                                            className="hidden group-hover:block"
+                                        >
+                                            <Pause />
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <div
+                                        className={
+                                            isPlaying(track) ? "text-green-400" : "text-zinc-200"
+                                        }
+                                    >
+                                        <span className="block w-5 text-center text-base group-hover:hidden">
+                                            {index + 1}
+                                        </span>
+                                        <button
+                                            type="button"
+                                            className="w-5 text-center text-base hidden group-hover:block"
+                                            onClick={() => handlePlayTrack(track)}
+                                        >
+                                            <Play />
+                                        </button>
+                                    </div>
+                                )}
                             </div>
                         </CustomTable.Cell>
                         <CustomTable.Cell className={`${isPlaying(track) ? "text-green-400" : ""}`}>
                             <TrackCell track={track} />
                         </CustomTable.Cell>
                         <CustomTable.Cell align="right">
-                            <div className="flex items-center justify-center space-x-3">
+                            <div className="flex items-center justify-end space-x-3">
                                 <button
                                     type="button"
                                     className={`${track.isSaved ? "text-green-500" : "invisible group-hover:visible text-zinc-400 hover:text-zinc-200 hover:scale-105 "}`}
@@ -348,10 +358,9 @@ const TopTracksTable = () => {
                                         <TickCircle variant="Bold" size={18} />
                                     )}
                                 </button>
-                                <span>
+                                <span className="text-[#b3b3b3]">
                                     {convertMillisecondsToMinutes(track.duration_ms)}
                                 </span>
-
                             </div>
                         </CustomTable.Cell>
                     </CustomTable.Row>
